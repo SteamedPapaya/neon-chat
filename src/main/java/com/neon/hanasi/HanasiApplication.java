313297557eb7d6b48df62aa933c1a67571944eac
siyoon <ryuu.public@gmail.com>
2024-09-17 11:50:15 +0900

    feat: add Application class file

---
+++ src/main/java/com/neon/hanasi/HanasiApplication.java
@@ -0,0 +1,13 @@
+package com.neon.hanasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanasiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanasiApplication.class, args);
+    }
+
+}
