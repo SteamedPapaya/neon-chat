692aaf9c4089997f6348b3fd178b38d5903076fc
siyoon <ryuu.public@gmail.com>
2024-09-17 11:53:13 +0900

    feat: initialize WebSocket configuration and basic handler

---
+++ src/main/java/com/neon/hanasi/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.neon.hanasi.config;
+
+import com.neon.hanasi.handler.ChatHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler(), "/chat").setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ChatHandler chatHandler() {
+        return new ChatHandler();
+    }
+}
